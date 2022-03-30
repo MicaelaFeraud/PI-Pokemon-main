fd2f5dc976384bf14e8d0476545739a184ef41a2
MicaelaFeraud <micaferaud@gmail.com>
2022-03-30 17:30:21 -0300

    Update

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -4,9 +4,50 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('pokemon', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+
+    idPokemon: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+    },
+
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+
+    //vida
+    health: {
+      type: DataTypes.INTEGER
+    },
+
+    //fuerza
+    strength: {
+      type: DataTypes.INTEGER
+    },
+
+    //defensa 
+    defense: {
+      type: DataTypes.INTEGER
+    },
+
+    //altura
+    height: {
+      type: DataTypes.DECIMAL
+    },
+
+    //peso
+    weight: {
+      type: DataTypes.DECIMAL
+    },
+
+    //velocidad
+    speed: {
+      type: DataTypes.INTEGER
+    }
   });
 };
